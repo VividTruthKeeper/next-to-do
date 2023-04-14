d3746163269cdace14c246f59d636af3cb96bd00
VividTruthKeeper <mr.aydogdy02@gmail.com>
2023-04-14 23:26:25 +0500

    app title

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,19 @@
 import { Inter } from "@next/font/google";
 
 const inter = Inter({ subsets: ["latin"] });
+
+import AppTitle from "@/components/AppTitle";
 import ThemeButton from "@/components/ThemeButton";
 
 const Home = () => {
   return (
     <main className="bg-[#F7ECDE] dark:bg-[#1C273C] transition-all min-h-screen min-w-max">
       <ThemeButton />
-      <div className="container"></div>
+      <div className="container">
+        <div className="inner pt-20 pb-8">
+          <AppTitle />
+        </div>
+      </div>
     </main>
   );
 };
---
+++ components/AppTitle.tsx
@@ -0,0 +1,9 @@
+const AppTitle = () => {
+  return (
+    <h1 className="text-[#E9C597] font-extrabold text-3xl text-center">
+      PERSONAL TASK MANAGER
+    </h1>
+  );
+};
+
+export default AppTitle;
